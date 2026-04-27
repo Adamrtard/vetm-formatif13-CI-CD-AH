898a78b1e03f80d7e049c5d8c539ad9cbb5d6c3a
adamrtard <2245981@cegeplimoilou.ca>
2026-04-27 10:33:37 -0400

    add: test qui ne passe pas

--- src/test/java/org/climoilou/vetm/MainTest.java
+++ src/test/java/org/climoilou/vetm/MainTest.java
@@ -3,11 +3,17 @@ package org.climoilou.vetm;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class MainTest {
 
     @Test
     void mainTest() {
         assertTrue(true);
+    }
+
+    @Test
+    void testQuiPassePas(){
+        fail();
     }
 }
